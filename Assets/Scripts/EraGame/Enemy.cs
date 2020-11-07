6d3f4a34305399e919212cd29a4edac75f0040aa
allernur2005 <66952849+allernur2005@users.noreply.github.com>
2020-11-07 16:21:05 +0600

    EraGame

---
+++ Assets/Scripts/EraGame/Enemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    public GameObject player;
+    public float dist;
+    NavMeshAgent nav;
+    public float Radius = 15;
+
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+
+    void Update()
+    {
+        dist = Vector3.Distance(player.transform.position, transform.position);
+        if (dist > Radius)
+        {
+            nav.enabled = false;
+        }
+        if (dist < Radius)
+        {
+            nav.enabled = true;
+            nav.SetDestination(player.transform.position);
+        }
+    }
+}
