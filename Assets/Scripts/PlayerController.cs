95c13db7c9b3948e9a380d43b95002a009974b19
Sergey Muhin <2chaosman@gmail.com>
2020-11-07 09:14:45 +0300

    add project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private float horizontalInput;
+    public GameObject bulletPrefab;
+    public int health = 5;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.left * speed * horizontalInput);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(bulletPrefab, transform.position, bulletPrefab.transform.rotation);
+        }
+    }
+}
