f855d5cec273e4d10c13a8dbdd4c131526ac9c61
Sergey Muhin <2chaosman@gmail.com>
2020-11-07 12:23:22 +0300

    add turel

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed;
+    public float speed = 3;
     private float horizontalInput;
-    public GameObject plasmaPrefab;
-    public GameObject laserPrefab;
+
     public int health = 5;
 
     // Start is called before the first frame update
@@ -23,36 +22,8 @@ public class PlayerController : MonoBehaviour
 
         if(transform.position.x < 13 && transform.position.x > -13)
         {
-            transform.Translate(Vector3.left * speed * horizontalInput);
+            transform.Translate(Vector3.right * speed * horizontalInput);
         }
-
-
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            ShootLaser();
-            //Instantiate(laserPrefab, transform.position, laserPrefab.transform.rotation);
-        }
-        if (Input.GetMouseButtonDown(1))
-        {
-            ShootPlasma();
-            //Instantiate(plasmaPrefab, transform.position, plasmaPrefab.transform.rotation);
-        }
-    }
-
-    void ShootLaser()
-    {
-        RaycastHit hit;
-        if(Physics.Raycast(transform.position, transform.forward, out hit))
-        {
-            Debug.Log(hit.transform.name);
-            Instantiate(laserPrefab, transform.position, laserPrefab.transform.rotation);
-        }
-    }
-
-    void ShootPlasma()
-    {
-
     }
 }
 
